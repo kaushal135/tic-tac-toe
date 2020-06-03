eed71ab31df24a4dc3b4ee337b0a93bcc74c8b2f
Kaushal Joshi <kaushal_1995@yahoo.ca>
2020-06-03 17:06:32 -0400

    Update index.js
    removed commented code

--- src/index.js
+++ src/index.js
@@ -57,23 +57,6 @@ class Board extends React.Component {
   render() {
     return (
       this.renderBoard()
-      // <div>
-      //   <div className="board-row">
-      //     {this.renderSquare(0)}
-      //     {this.renderSquare(1)}
-      //     {this.renderSquare(2)}
-      //   </div>
-      //   <div className="board-row">
-      //     {this.renderSquare(3)}
-      //     {this.renderSquare(4)}
-      //     {this.renderSquare(5)}
-      //   </div>
-      //   <div className="board-row">
-      //     {this.renderSquare(6)}
-      //     {this.renderSquare(7)}
-      //     {this.renderSquare(8)}
-      //   </div>
-      // </div>
     );
   }
 }
